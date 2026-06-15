f6e25f258728f7aa1e5cc65c2ac186dbcdd8f7f2
Murilo <oxelot17898@gmail.com>
2026-06-15 13:00:54 -0300

    Atualizando login no front sem enviar o nível de acesso

--- Frontend/web_adm/src/js/login.js
+++ Frontend/web_adm/src/js/login.js
@@ -2,13 +2,12 @@ const nome = document.getElementById("nome")
 const senha = document.getElementById("senha")
 const email = document.getElementById("email")
 const entrar = document.getElementById("entrar")
-const nivel_acesso = 1
 
-const login = async (nome, email, senha, nivel_acesso) => {
+const login = async (nome, email, senha) => {
     const OPTIONS = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ nome, email, senha, nivel_acesso })
+        body: JSON.stringify({ nome, email, senha })
     }
 
     const response = await fetch("http://localhost:8080/v1/sorvetudos/admin/auth/login", OPTIONS)
@@ -18,14 +17,13 @@ const login = async (nome, email, senha, nivel_acesso) => {
 }
 
 const fazerLogin = async () => {
-    const { response, data } = await login(nome.value, email.value, senha.value, nivel_acesso)
+    const { response, data } = await login(nome.value, email.value, senha.value)
 
     if (response.ok) {
         localStorage.setItem('token', data.response.auth[0].token)
-        window.location.href = "dashboard_modelo.html" // redireciona na hora
+        window.location.href = "dashboard_modelo.html"
     } else {
         console.error("Login inválido:", data)
-        // aqui você pode mostrar uma mensagem de erro pro usuário
     }
 }
 
